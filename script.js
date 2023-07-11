54ab3164efee46d623e8970d4ff09e5d503573e9
jpmunich <parkercoyle8@gmail.com>
2023-07-11 16:03:56 -0400

    Functionality to addBookScreen + library array Clicking the add book button will cause a new screen to come up that prompts the user to enter their new book in. When submit is pressed, the program breaks and the new book is pushed to the library array on the server side.

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const button = document.querySelector('button');
+const addBookButton = document.querySelector('.add-book');
+const submitButton = document.querySelector('#submit');
+const addBookScreen = document.querySelector('.add-book-screen');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const hasRead = document.querySelector('#has-read');
+const overlay = document.querySelector('.overlay');
 
 let library = [];
 
@@ -16,7 +23,17 @@ function addBookToLibrary(title, author, pages, hasRead) {
     console.log(library)
 }
 
-button.addEventListener('click', () => {addBookToLibrary("a", "b", 'c', 'd')})
+addBookButton.addEventListener('click', () => {
+    addBookScreen.classList.remove('disappear');
+    overlay.classList.remove('disappear');
+})
+
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    addBookScreen.classList.add('disappear');
+    overlay.classList.add('disappear');
+    addBookToLibrary(title.value, author.value, pages.value, hasRead.isChecked);
+})
 
 function updateLibrary() {
     
