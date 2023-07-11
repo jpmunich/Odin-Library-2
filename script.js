283ad5ac84513a8499786e5ef978aa755d9e9653
jpmunich <parkercoyle8@gmail.com>
2023-07-11 15:29:32 -0400

    Function that adds to library on button click

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const button = document.querySelector('button');
+
 let library = [];
 
 function Book(title, author, pages, hasRead) {
@@ -8,4 +10,15 @@ function Book(title, author, pages, hasRead) {
     this.info = function() {console.log(`This book, ${this.title}, by ${this.author} has ${this.pages} pages. It is ${this.hasRead} that I have read it!`)}
 }
 
+function addBookToLibrary(title, author, pages, hasRead) {
+    const newBook = new Book(title, author, pages, hasRead);
+    library.push(newBook);
+    console.log(library)
+}
+
+button.addEventListener('click', () => {addBookToLibrary("a", "b", 'c', 'd')})
+
+function updateLibrary() {
+    
+}
 
